6efa08f1e0a8ebef2af800caf2ea9085f849fab9
John Gunderman <johngunderman@gmail.com>
2014-12-24 15:48:25 -0800

    Initial run at some basic test code on models

---
+++ test/modules/test_model.py
@@ -0,0 +1,37 @@
+import unittest
+
+from google.appengine.api import memcache
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
+
+from storage.models import Node, Workflow, Task
+
+class ModelTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def testModelWorkflow_to_json(self):
+        workflow = Workflow(name='test',
+                            description='foo',
+                            components=[ndb.Key('Node', 1)])
+        workflow.put()
+        workflow.to_json()
+
+    def testModelTask_to_json(self):
+        task = Task(name='test',
+                    workflow='test workflow',
+                    active_nodes=[ndb.Key('Node', 1)],
+                    metadata='test metadata')
+
+if __name__ == '__main__':
+    unittest.main()
